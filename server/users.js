0029d9abbc9d66330bb458933aa1d9b470abde46
Andrei Constantinescu <andrei@constantinescu.io>
2015-04-16 19:12:43 +0300

    Add Meteor cignUp user method.

---
+++ server/users.js
@@ -0,0 +1,32 @@
+Meteor.methods ({
+  singupUser: function (opts) {
+    var opts = _.pick (opts, 'username, password, firstName, lastName, email');
+    var emailRegxp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailRegxp.test(opts.username))
+      throw new Meteor.Error(403, 'Username must be 3-30 characters.');
+
+    if (opts.password.length < 6 || opts.password.length > 32)
+      throw new Meteor.Error(403, 'Password must be 3-32 characters.');
+
+    if (opts.firstName.length < 6 || opts.firstName.length > 32)
+      throw new Meteor.Error(403, 'First Name must be 3-32 characters.');
+
+    if (opts.lastName.length < 6 || opts.lastName.length > 32)
+      throw new Meteor.Error(403, 'Last Name must be 3-32 characters.');
+
+    if (opts.email.length > 256)
+      throw new Meteor.Error(403, 'Email must be lower than 256 characters.');
+
+  var newUser = Accounts.createUser ({
+    username: opts.username,
+    password: opts.password,
+    email: opts.email,
+    profile: {
+      firstName: opts.firstName,
+      lastName: opts.lastName
+    }
+  });
+  return true;
+  }
+});
